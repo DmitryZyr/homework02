5a5b957f390836db0aa6f450edc94f609dea866c
zyrainovdv <zyrianovdv@skbkontur.ru>
2018-10-08 21:24:19 +0500

    homework

--- src/main/scala/fintech/homework02/ComplexNumber.scala
+++ src/main/scala/fintech/homework02/ComplexNumber.scala
@@ -1,10 +1,45 @@
 package fintech.homework02
 
-class ComplexNumber {
-  ???
+class ComplexNumber (val re: Double, val im: Double = 0.0) {
 
-  // Написать класс описывающий комплексные числа.
-  // Реализовать проверку на равенство, умножение и сложение, toString.
-  // Реализовать оператор возведения в целую степень: "~".
-  // Реализовать тесты в ComplexNumberSpec
+  def +(c: ComplexNumber): ComplexNumber = new ComplexNumber(re + c.re, im + c.im)
+
+  def *(c: ComplexNumber): ComplexNumber = new ComplexNumber(re * c.re - im * c.im, re * c.im + im * c.re)
+
+  def ~(r: Double): ComplexNumber = {
+    val (rad, ang) = polar
+    new ComplexNumber(math.pow(rad, r), ang * r)
+  }
+
+  def radius: Double = math.sqrt(math.pow(re, 2) + math.pow(im, 2))
+
+  def polar: (Double, Double) = {
+    val rad = radius
+    (rad, Math.acos(re / rad))
+  }
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[ComplexNumber]
+
+  override def equals(other: Any): Boolean = other match {
+    case that: ComplexNumber =>
+      (that canEqual this) &&
+        re == that.re &&
+        im == that.im
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(re, im)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
+
+  override def toString: String = {
+    val str = if (re == 0 && im == 0) "0"
+    else if (re == 0) im + " * i"
+    else if (im == 0) re.toString
+    else if (im == 1) re + " + i"
+    else if (im == -1) re + " - i"
+    else re + (if (im < 0) " - " + -im else " + " + im) + " * i"
+    str
+  }
 }
--- src/test/scala/fintech/homework02/ComplexNumberSpec.scala
+++ src/test/scala/fintech/homework02/ComplexNumberSpec.scala
@@ -2,5 +2,63 @@ package fintech.homework02
 import org.scalatest.{FlatSpec, Matchers}
 
 class ComplexNumberSpec extends FlatSpec with Matchers {
-  ???
+  implicit def extendComplexNumber(i: ComplexNumber) = new ExtendedComplexNumber(i)
+
+  "ComplexNumber" should "toString correctly" in {
+    new ComplexNumber(0, 0).toString shouldEqual "0"
+    new ComplexNumber(1).toString shouldEqual "1.0"
+    new ComplexNumber(1, 0).toString shouldEqual "1.0"
+    new ComplexNumber(1, 1).toString shouldEqual "1.0 + i"
+    new ComplexNumber(1, 2).toString shouldEqual "1.0 + 2.0 * i"
+    new ComplexNumber(-1, 2).toString shouldEqual "-1.0 + 2.0 * i"
+    new ComplexNumber(-1, -2).toString shouldEqual "-1.0 - 2.0 * i"
+    new ComplexNumber(0, -2).toString shouldEqual "-2.0 * i"
+    new ComplexNumber(0.23, 2.48).toString shouldEqual "0.23 + 2.48 * i"
+    new ComplexNumber(-0.23, -2.48).toString shouldEqual "-0.23 - 2.48 * i"
+  }
+
+  "ComplexNumber" should "equal correctly" in {
+    new ComplexNumber(2, 2) should === (new ComplexNumber(2, 2))
+    new ComplexNumber(0, 0) shouldEqual new ComplexNumber(0, 0)
+    new ComplexNumber(1, 1) shouldEqual new ComplexNumber(1, 1)
+    new ComplexNumber(-1, -1) shouldEqual new ComplexNumber(-1, -1)
+    new ComplexNumber(1.53, 2.76) shouldEqual new ComplexNumber(1.53, 2.76)
+    new ComplexNumber(-1.53, -2.76) shouldEqual new ComplexNumber(-1.53, -2.76)
+  }
+
+  "ComplexNumber" should "sum correctly" in {
+    new ComplexNumber(0, 0) + new ComplexNumber(0, 0) shouldEqual new ComplexNumber(0, 0)
+    new ComplexNumber(1) + new ComplexNumber(2) shouldEqual new ComplexNumber(3)
+    new ComplexNumber(1, 0) + new ComplexNumber(2, 0) shouldEqual new ComplexNumber(3, 0)
+    new ComplexNumber(1, 1) + new ComplexNumber(2, 2) shouldEqual new ComplexNumber(3, 3)
+    new ComplexNumber(-1, -1) + new ComplexNumber(-2, -2) shouldEqual new ComplexNumber(-3, -3)
+    new ComplexNumber(0, -1) + new ComplexNumber(0, -2) shouldEqual new ComplexNumber(0, -3)
+    new ComplexNumber(1, -1) + new ComplexNumber(2, -2) shouldEqual new ComplexNumber(3, -3)
+    new ComplexNumber(-1, 1) + new ComplexNumber(-2, 2) shouldEqual new ComplexNumber(-3, 3)
+    new ComplexNumber(0.23, 2.48) + new ComplexNumber(7.12, 6.54) shouldEqualWithEps new ComplexNumber(7.35, 9.02)
+    new ComplexNumber(-0.23, -2.48) + new ComplexNumber(-7.12, -6.54) shouldEqualWithEps new ComplexNumber(-7.35, -9.02)
+    new ComplexNumber(0.23, -2.48) + new ComplexNumber(-7.12, 6.54) shouldEqualWithEps new ComplexNumber(-6.89, 4.06)
+  }
+
+  "ComplexNumber" should "multiply correctly" in {
+    new ComplexNumber(0, 0) * new ComplexNumber(0, 0) shouldEqual new ComplexNumber(0, 0)
+    new ComplexNumber(0, 0) * new ComplexNumber(0.48, 6.84) shouldEqual new ComplexNumber(0, 0)
+    new ComplexNumber(3.22, -8.90) * new ComplexNumber(0, 0) shouldEqual new ComplexNumber(0, 0)
+    new ComplexNumber(1) * new ComplexNumber(2) shouldEqual new ComplexNumber(2)
+    new ComplexNumber(1, 0) * new ComplexNumber(2, 0) shouldEqual new ComplexNumber(2, 0)
+    new ComplexNumber(1, 1) * new ComplexNumber(2, 2) shouldEqual new ComplexNumber(0, 4)
+    new ComplexNumber(-1, -1) * new ComplexNumber(-2, -2) shouldEqual new ComplexNumber(0, 4)
+    new ComplexNumber(0, -1) * new ComplexNumber(0, -2) shouldEqual new ComplexNumber(-2, 0)
+    new ComplexNumber(1, -1) * new ComplexNumber(2, -2) shouldEqual new ComplexNumber(0, -4)
+    new ComplexNumber(-1, 1) * new ComplexNumber(-2, 2) shouldEqual new ComplexNumber(0, -4)
+    new ComplexNumber(0.23, 2.48) * new ComplexNumber(7.12, 6.54) shouldEqualWithEps new ComplexNumber(-14.5816, 19.1618)
+    new ComplexNumber(-0.23, -2.48) * new ComplexNumber(-7.12, -6.54) shouldEqualWithEps new ComplexNumber(-14.5816, 19.1618)
+    new ComplexNumber(-0.23, 2.48) * new ComplexNumber(7.12, -6.54) shouldEqualWithEps new ComplexNumber(14.5816, 19.1618)
+  }
+}
+class ExtendedComplexNumber(value: ComplexNumber) {
+  def shouldEqualWithEps(expected: ComplexNumber, eps: Double = 0.0001) = {
+    assert(math.abs(value.re - expected.re) < eps, s"eps $eps was exceeded. value: ${value.re}, expected: ${expected.re}")
+    assert(math.abs(value.im - expected.im) < eps, s"eps $eps was exceeded. value: ${value.im}, expected: ${expected.im}")
+  }
 }
